88a1b2c9b4f975bae838a5f3eb46bf986831cff6
Josh <josh1billion@gmail.com>
2016-02-02 18:47:23 -0600

    Updated sample input

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var tabsToNotes = require('tabs-to-notes');
 
-var sampleInput = `|123
-|1--
-|-3-
-|---
-|--2
-|---`;
+var sampleInput = `---------------------|
+---------------------|
+-9-9-9-9-9-9-9-9-9-9-|
+-9-9-9-9-9-9-9-9-9-9-|
+-7-7-7-7-7-7-7-7-7-7-|
+---------------------|`;
 
 var notes = tabsToNotes(sampleInput);
 console.log(notes);
